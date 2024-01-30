245a9d676db8233b30d822cdc04e800dec61abbb
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2024-01-30 09:45:23 +0200

    Sidebar added

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import './Sidebar.scss';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-function sidebar() {
-  return (
-    <div>sidebar</div>
-  )
-}
+const Sidebar = () => {
+	const { auth } = useSelector((state) => ({ ...state }));
+	const { currentUser } = auth;
 
-export default sidebar
+	return (
+		<div>
+			<ul className='sidebar'>
+				<li className='list-item'>
+					<h5>{currentUser.username}</h5>
+				</li>
+				<li className='list-item'>
+					<Link to='/dashboard'>Dashboard</Link>
+				</li>
+				<li className='list-item'>
+					<Link to='/settings'>Settings</Link>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Sidebar;
