d98919e45b1df26d0687e2cf0964ad694a53d171
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2024-02-01 11:37:22 +0200

    Header styled

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,23 +1,10 @@
-import React from "react";
+
 import "./header.scss";
-import "../../styles/components/_button.scss";
 import logo from "../../assets/images/logo.png";
-import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { logoutSuccess } from '../../redux/authSlice';
-import history from "../../history";
+
 
 function Header() {
-  const dispatch = useDispatch();
-  const { auth } = useSelector((state) => ({ ...state }));
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    dispatch(logoutSuccess());
-    localStorage.removeItem("auth");
-    history.push("/signin");
-    window.location.reload();
-  };
+  
   return (
     <div>
       <nav className="header">
