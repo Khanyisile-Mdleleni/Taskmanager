76c72b8d8689ea3133cbc91f302b373d73b3d506
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2024-02-01 12:22:41 +0200

    Updates on sidebar

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,32 +1,25 @@
-import './Sidebar.scss';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import "./Sidebar.scss";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-	const { auth } = useSelector((state) => ({ ...state }));
-	const { currentUser } = auth;
-
-	return (
-		<div>
-			<ul className='sidebar'>
-				{/* <li className='list-item'>
-					<h5>Welcome {currentUser.username}</h5>
-				</li> */}
-				<li className='list-item'>
-					<Link to='/'>Home</Link>
-				</li>
-				<li className='list-item'>
-					<Link to='/settings'>Tasks</Link>
-				</li>
-        <li className='list-item'>
-					<Link to='/dashboard'>Dashboard</Link>
-				</li>
-				<li className='list-item'>
-					<Link to='/help'>Help</Link>
-				</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div>
+      <ul className="sidebar">
+        <li className="list-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="list-item">
+          <Link to="/settings">Tasks</Link>
+        </li>
+        <li className="list-item">
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li className="list-item">
+          <Link to="/help">Help</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
