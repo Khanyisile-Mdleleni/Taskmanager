e159ad9203b390ad6a03aea2ca4774b62689211c
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2024-02-01 11:12:20 +0200

    Styled contact page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,60 +1,65 @@
-import './App.css'
-import Header from './components/Header/Header'
-import SignUp from './components/registration/SignUp'
-import SignIn from './components/registration/SignIn';
-import Home from './pages/Home/Home';
-import Dashboard from './pages/dashboard/Dashboard'
-import Help from './pages/Help/help'
+import "./App.css";
+import Header from "./components/Header/Header";
+import SignUp from "./components/registration/SignUp";
+import SignIn from "./components/registration/SignIn";
+import Home from "./pages/Home/Home";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Help from "./pages/Help/help";
+import Contact from "./pages/Contact/Contact";
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './styles/main.scss';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./styles/main.scss";
 
-import TaskManager from './pages/taskmanagement/TaskManager';
-import RequireAuth from './utils/RequireAuth';
-import { useSelector } from 'react-redux';
-
+import TaskManager from "./pages/taskmanagement/TaskManager";
+import RequireAuth from "./utils/RequireAuth";
+import { useSelector } from "react-redux";
 
 function App() {
-	const { auth } = useSelector((state) => ({ ...state }));
+  const { auth } = useSelector((state) => ({ ...state }));
   return (
     <>
-   <Router>
-				<Header />
-				<Routes>
-        <Route path='/' element={<Home />} />
-					<Route
-						path='/signin'
-						element={<SignIn />}
-					/>
-					<Route
-						path='/signup'
-						element={<SignUp/>}
-					/>
-					<Route
-						path='/taskmanager'
-						element={
-							<RequireAuth>
-								<TaskManager />
-							</RequireAuth>
-						}
-					/>
-					<Route
-						path='/dashboard'
-						element={
-							<RequireAuth><Dashboard/>
-							</RequireAuth>}
-					/>
-					<Route
-						path='/help'
-						element={
-							<RequireAuth><Help/>
-							</RequireAuth>}
-					/>
-				</Routes>
-				
-			</Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/taskmanager"
+            element={
+              <RequireAuth>
+                <TaskManager />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <RequireAuth>
+                <Dashboard />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/help"
+            element={
+              <RequireAuth>
+                <Help />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <RequireAuth>
+                <Contact />
+              </RequireAuth>
+            }
+          />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Contact/Contact.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { Form, Input, Button, message } from "antd";
+import "./contact.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+const { TextArea } = Input;
+
+function Contact() {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+    message.success("Message sent successfully");
+    form.resetFields(); // Reset the form fields
+  };
+
+  const formItemLayout = {
+    labelCol: {
+      span: 24,
+    },
+    wrapperCol: {
+      span: 24,
+    },
+  };
+
+  return (
+    <>
+     
+      <div className="dashboard">
+        <div className="dashboard__left">
+          <Sidebar />
+        </div>
+        <div className="dashboard__right">
+        <h1 className="title">Contact Us</h1>
+      <div className="contact">
+        <p>Need assistance? Submit a request below and we will get to work!</p>
+      </div>
+          <div className="form">
+            <Form
+              form={form}
+              name="register"
+              onFinish={onFinish}
+              style={{ maxWidth: 400 }}
+            >
+              <Form.Item
+                {...formItemLayout}
+                label="E-mail"
+                name="email"
+                rules={[
+                  {
+                    type: "email",
+                    message: "Please enter a valid email address.",
+                  },
+                  {
+                    required: true,
+                    message: "Please input your E-mail.",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                {...formItemLayout}
+                label="Message"
+                name="message"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your message.",
+                  },
+                ]}
+              >
+                <TextArea rows={4} />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Send Message
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Contact;
