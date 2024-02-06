617ec25c8b4ee0b7448122d02d2c2e4f4c70288c
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2024-02-06 10:24:44 +0200

    modified the task page

--- client/src/pages/taskmanagement/TaskManager.jsx
+++ client/src/pages/taskmanagement/TaskManager.jsx
@@ -6,12 +6,14 @@ import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import AssignmentOutlinedIcon from "@mui/icons-material/AssignmentOutlined";
 import TableChartOutlinedIcon from "@mui/icons-material/TableChartOutlined";
-import PictureAsPdfOutlinedIcon from '@mui/icons-material/PictureAsPdfOutlined';
-import DriveFileRenameOutlineOutlinedIcon from '@mui/icons-material/DriveFileRenameOutlineOutlined';
-import Files from "../Files/Files"
-import Notes from '../Notes/Notes';
-import Tasks from '../Tasks/Tasks';
-import Board from '../Board/Board';
+import PictureAsPdfOutlinedIcon from "@mui/icons-material/PictureAsPdfOutlined";
+import DriveFileRenameOutlineOutlinedIcon from "@mui/icons-material/DriveFileRenameOutlineOutlined";
+import Files from "../Files/Files";
+import Notes from "../Notes/Notes";
+import Tasks from "../Tasks/Tasks";
+import Board from "../Board/Board";
+import { useSelector } from "react-redux";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 function taskmanager() {
   const [value, setValue] = React.useState("1");
@@ -19,43 +21,78 @@ function taskmanager() {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  const { auth } = useSelector((state) => ({ ...state }));
+  const { currentUser } = auth;
   return (
     <>
-         <h4 className="title">
-        Welcome Back !
-        <span id="wave" role="img" aria-labelledby="wave">
-          👋🏻
-        </span>{" "}
-      </h4>
-    <div className="Tasks">
-      <Box sx={{ width: "100%", typography: "body1" }}>
-        <TabContext value={value}> 
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <TabList onChange={handleChange} aria-label="lab API tabs example" >
-              <Tab icon={<AssignmentOutlinedIcon />}  iconPosition="start" label="Tasks" value="1" />
-              <Tab icon={<TableChartOutlinedIcon />} iconPosition="start" label="Board" value="2" style={{marginLeft: "100px"}}/>
-              <Tab icon={<PictureAsPdfOutlinedIcon />} iconPosition="start" label="Files" value="3" style={{marginLeft: "100px"}}/>
-              <Tab icon={<DriveFileRenameOutlineOutlinedIcon  />} iconPosition="start" label="Notes" value="4" style={{marginLeft: "100px"}} />
-            </TabList>
-          </Box>
-          <TabPanel value="1">
-          <Tasks />
-          </TabPanel>
-          <TabPanel value="2">
-          <Board />
-          </TabPanel>
-          <TabPanel value="3">
-            <Files />
-          </TabPanel>
-          <TabPanel value="4">
-            <Notes />
-          </TabPanel>
-        </TabContext>
-      </Box>
-    </div>
+      <div className="dashboard">
+        <div className="dashboard__left">
+          <Sidebar />
+        </div>
+        <div className="dashboard__right">
+          <div className="dashboard__rightContent"></div>
+          <h4 className="title">
+            Welcome Back {currentUser.username}!
+            <span id="wave" role="img" aria-labelledby="wave">
+              👋🏻
+            </span>{" "}
+          </h4>
+          <div className="Tasks">
+            <Box sx={{ width: "100%", typography: "body1" }}>
+              <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                  <TabList
+                    onChange={handleChange}
+                    aria-label="lab API tabs example"
+                  >
+                    <Tab
+                      icon={<AssignmentOutlinedIcon />}
+                      iconPosition="start"
+                      label="Tasks"
+                      value="1"
+                    />
+                    <Tab
+                      icon={<TableChartOutlinedIcon />}
+                      iconPosition="start"
+                      label="Board"
+                      value="2"
+                      style={{ marginLeft: "100px" }}
+                    />
+                    <Tab
+                      icon={<PictureAsPdfOutlinedIcon />}
+                      iconPosition="start"
+                      label="Files"
+                      value="3"
+                      style={{ marginLeft: "100px" }}
+                    />
+                    <Tab
+                      icon={<DriveFileRenameOutlineOutlinedIcon />}
+                      iconPosition="start"
+                      label="Notes"
+                      value="4"
+                      style={{ marginLeft: "100px" }}
+                    />
+                  </TabList>
+                </Box>
+                <TabPanel value="1">
+                  <Tasks />
+                </TabPanel>
+                <TabPanel value="2">
+                  <Board />
+                </TabPanel>
+                <TabPanel value="3">
+                  <Files />
+                </TabPanel>
+                <TabPanel value="4">
+                  <Notes />
+                </TabPanel>
+              </TabContext>
+            </Box>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
 
 export default taskmanager;
-
